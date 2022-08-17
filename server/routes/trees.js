370e9d3b66bbee998a3bc5710686aff1b08cd0b9
labor-day <shaffer.scott@yahoo.com>
2022-08-17 16:56:50 -0600

    server executing SQL commands

--- server/routes/trees.js
+++ server/routes/trees.js
@@ -8,6 +8,12 @@ const router = express.Router();
  *   - Database permissions: read/write records in tables
  */
 // Your code here
+const DATA_SOURCE = process.env.data_source;
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(DATA_SOURCE, sqlite3.OPEN_READWRITE);
+let sql;
+let params;
+const successMsg = {"message": "success"};
 
 /**
  * BASIC PHASE 2, Step B - List of all trees in the database
@@ -21,6 +27,17 @@ const router = express.Router();
  */
 // Your code here
 
+const getTrees = (req, res, next) => {
+  sql = `SELECT id, tree FROM trees ORDER by tree ASC`;
+  params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) return next(err);
+    res.json(rows);
+  });
+}
+
+router.get('/', getTrees);
+
 /**
  * BASIC PHASE 3 - Retrieve one tree with the matching id
  *
@@ -31,6 +48,17 @@ const router = express.Router();
  *   - Properties: id, tree, location, height_ft, ground_circumference_ft
  */
 // Your code here
+const getTreeById = (req, res, next) => {
+  sql = `SELECT * FROM trees WHERE id=?`;
+  params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) return next(err);
+    res.json(row);
+  });
+
+};
+
+router.get('/:id', getTreeById);
 
 /**
  * INTERMEDIATE PHASE 4 - INSERT tree row into the database
@@ -43,6 +71,19 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+const insertTree = (req, res, next) => {
+  sql =
+  `INSERT INTO
+  trees (tree, location, height_ft, ground_circumference_ft)
+  VALUES (?, ?, ?, ?)`
+  params = [req.body.name, req.body.location, req.body.height, req.body.size];
+  db.run(sql, params, (err, row) => {
+    if (err) return next(err);
+    res.json(successMsg);
+  })
+}
+
+router.post('/', insertTree);
 
 /**
  * INTERMEDIATE PHASE 5 - DELETE a tree row from the database
@@ -55,6 +96,16 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+const deleteTreeById = (req, res, next) => {
+  sql = `DELETE FROM trees WHERE id=?`;
+  params = [req.params.id];
+  db.run(sql, params, (err, row) => {
+    if (err) return next(err);
+    res.json(successMsg);
+  });
+}
+
+router.delete('/:id', deleteTreeById);
 
 /**
  * INTERMEDIATE PHASE 6 - UPDATE a tree row in the database
@@ -67,6 +118,28 @@ const router = express.Router();
  *   - Value: success
  */
 // Your code here
+const checkTreeId = (req, res, next) => {
+  if (req.body.id != req.params.id) {
+    let error = new Error("ids do not match");
+    error.statusCode = 400;
+    next(error);
+  }
+  next();
+}
+
+const updateTreeById = (req, res, next) => {
+  sql =
+  `UPDATE trees
+  SET tree=?, location=?, height_ft=?, ground_circumference_ft=?
+  WHERE id=?`;
+  params = [req.body.name, req.body.location, req.body.height, req.body.size, req.body.id];
+  db.run(sql, params, (err, row) => {
+    if (err) return next(err);
+    res.json(successMsg);
+  });
+
+}
+router.put('/:id', checkTreeId, updateTreeById);
 
 // Export class - DO NOT MODIFY
 module.exports = router;
--- server/seed-data.sql
+++ server/seed-data.sql
@@ -1,5 +1,23 @@
 -- BASIC PHASE 1A - DROP and CREATE table
 -- Your code here
+DROP TABLE IF EXISTS trees;
 
 -- BASIC PHASE 1B - INSERT seed data
 -- Your code here
+
+CREATE TABLE trees(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tree VARCHAR(32),
+  location VARCHAR(64),
+  height_ft FLOAT,
+  ground_circumference_ft FLOAT
+);
+
+INSERT INTO
+TREES (tree, location, height_ft, ground_circumference_ft)
+VALUES
+('General Sherman', 'Sequoia National Park', 274.9, 102.6),
+('General Grant', 'Kings National Park', 268.1, 107.5),
+('President', 'Sequioa National Park', 240.9, 93.0),
+('Lincoln', 'Sequioa National Park', 255.8, 98.3),
+('Stagg', 'Private Land', 243.0, 109.0);
